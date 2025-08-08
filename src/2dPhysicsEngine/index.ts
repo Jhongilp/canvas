d36ade3863d43eef0b85f55d886dc714d94d17fb
Jonathan Gil <jhongilp@gmail.com>
2025-08-08 12:23:51 -0500

    add acceleration

--- src/2dPhysicsEngine/index.ts
+++ src/2dPhysicsEngine/index.ts
@@ -1,3 +1,4 @@
+import { PIXELS_PER_METER } from "./physics/constants";
 import { Particle } from "./physics/particle";
 import { Vec2 } from "./physics/vec2";
 
@@ -12,18 +13,16 @@ export const setup2dPhysicsEngine = (canvas: HTMLCanvasElement) => {
   };
   ctx.translate(OFFSET.x, OFFSET.y);
 
-  const particle = new Particle(
-    Math.random() * 800 - 400,
-    Math.random() * 600 - 300,
-    1
-  );
-
+  const particle = new Particle(100, 50, 5);
+  
   let timePreviousFrame = performance.now();
   const update = () => {
     const deltaTime = (performance.now() - timePreviousFrame) / 1000;
     timePreviousFrame = performance.now();
 
-    particle.velocity = new Vec2(100 * deltaTime, 30 * deltaTime);
+    particle.acceleration = new Vec2(0, 9.8 * PIXELS_PER_METER); // gravity
+    particle.acceleration.scale(deltaTime);
+    particle.velocity.add(particle.acceleration);
     particle.position.add(particle.velocity);
 
     ctx.clearRect(-OFFSET.x, -OFFSET.y, canvas.width, canvas.height);
--- src/2dPhysicsEngine/physics/constants.ts
+++ src/2dPhysicsEngine/physics/constants.ts
@@ -1,2 +1,3 @@
 export const FPS = 60; // Frames per second for the physics engine
 export const MILLISEC_PER_FRAME = 1000 / FPS; // Milliseconds per frame
+export const PIXELS_PER_METER = 0.1;
